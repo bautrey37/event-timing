aae50bc9b727fb59db6027a1cba0bace65343179
bautrey37 <brandon.c.autrey@gmail.com>
2021-07-06 22:55:01 +0300

    install prettier

--- README.md
+++ README.md
@@ -3,11 +3,13 @@
 ## Build and Run
 
 Docker-compose:
+
 ```bash
 docker-compose up
 ```
 
 Dockerfile:
+
 ```bash
 docker build . -t event-timing
 docker run -p 3000:3000 -d event-timing
--- server.js
+++ server.js
@@ -1,10 +1,10 @@
-const Koa = require('koa');
+const Koa = require("koa");
 const app = new Koa();
 
 const PORT = 3000;
 
-app.use(async ctx => {
-  ctx.body = 'Hello World';
+app.use(async (ctx) => {
+  ctx.body = "Hello World";
 });
 
-app.listen(PORT, () => console.log('Server is ready'));
+app.listen(PORT, () => console.log("Server is ready"));
